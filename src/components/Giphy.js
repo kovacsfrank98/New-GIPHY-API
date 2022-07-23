b426bfa1422bc13d1ff3f79438e1faf2f97d6775
kovacsfrank98 <51347588+kovacsfrank98@users.noreply.github.com>
2022-07-22 20:57:34 -0400

    U6: Additional addityions

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -3,19 +3,28 @@ import axios from 'axios';
 
 function Search()
 {
+    const [data, setData] = useState({imgs:[]});
     const [query, setQuery] = useState('');
     
     useEffect(() => {
+     const fetchData = async() =>{
        const result = await axios(
        `https://api.giphy.com/v1/gifs/search?api_key=XNuamkpe9ftC5CK06o75Ye0MR4VkB0gH&q=${query}&limit=10&offset=0&rating=g&lang=en`
        );
-    });
+         setData(result.data);
+     };
+        fetchData();
+    }, []);
     
     return (
+        <div>
         <div className="Input">
             <h1>NEW GIPHY API</h1>
         </div>
+        </div>
+        <div>
         <div className="Display">
+        </div>
         </div>
     );
 }
