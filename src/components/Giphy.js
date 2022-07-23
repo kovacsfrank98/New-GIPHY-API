530c9006b23cec86bcc11861e5c04c33e1007161
kovacsfrank98 <51347588+kovacsfrank98@users.noreply.github.com>
2022-07-23 02:37:31 -0400

    Update 7.1

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -5,12 +5,13 @@ function Search()
 {
     const [data, setData] = useState({imgs:[]});
     const [query, setQuery] = useState('');
+    const [url, setUrl] = useState(
+        'https://api.giphy.com/v1/gifs/search?api_key=XNuamkpe9ftC5CK06o75Ye0MR4VkB0gH&q'
+    );
     
     useEffect(() => {
      const fetchData = async() =>{
-       const result = await axios(
-       `https://api.giphy.com/v1/gifs/search?api_key=XNuamkpe9ftC5CK06o75Ye0MR4VkB0gH&q=${query}&limit=10&offset=0&rating=g&lang=en`
-       );
+       const result = await axios(url);
          setData(result.data);
      };
         fetchData();
@@ -20,13 +21,25 @@ function Search()
         <div>
         <div className="Input">
             <h1>NEW GIPHY API</h1>
+            <p>Please enter a search topic:</p><br/>
+            <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+            <button
+                type="button"
+                onClick={() => 
+                         setUrl('https://api.giphy.com/v1/gifs/search?api_key=XNuamkpe9ftC5CK06o75Ye0MR4VkB0gH&q' + query)
+                        }>
+                SEARCH
+            </button>
         </div>
-        </div>
-        <div>
+        <div> 
         <div className="Display">
+                {data.imgs.map(item =>(
+                 <img className="Images"></img>
+                 ))}
+        </div>
         </div>
         </div>
     );
 }
 
-export default Giphy;
+export default Search;
