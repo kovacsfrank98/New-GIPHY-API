1ab76465d66036f3b8792738fdcd8ef527beb625
kovacsfrank98 <51347588+kovacsfrank98@users.noreply.github.com>
2022-07-27 10:25:25 -0400

    U8: new

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -4,10 +4,8 @@ import axios from 'axios';
 function Search()
 {
     const [data, setData] = useState({imgs:[]});
-    const [query, setQuery] = useState('');
-    const [url, setUrl] = useState(
-        'https://api.giphy.com/v1/gifs/search?api_key=XNuamkpe9ftC5CK06o75Ye0MR4VkB0gH&q'
-    );
+    const [query, setQuery] = useState('random');
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=XNuamkpe9ftC5CK06o75Ye0MR4VkB0gH&q=${query}&limit=10&offset=0&rating=g&lang=en`;
     
     useEffect(() => {
      const fetchData = async() =>{
@@ -15,7 +13,7 @@ function Search()
          setData(result.data);
      };
         fetchData();
-    }, []);
+    }, [url]);
     
     return (
         <div>
@@ -25,18 +23,19 @@ function Search()
             <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
             <button
                 type="button"
-                onClick={() => 
-                         setUrl('https://api.giphy.com/v1/gifs/search?api_key=XNuamkpe9ftC5CK06o75Ye0MR4VkB0gH&q' + query)
+                onClick={() =>
+                         fetch(url).then(response => response.json())
+                         .then(json => {
+                         json.data.map(imgs => imgs.images.fixed_height.url)
+                         .forEach(url => {
+                         <div className="Display">
+                            <img className="Images" src={data.url}></img>
+                         </div>
+                        })
+                        })
                         }>
                 SEARCH
             </button>
-        </div>
-        <div> 
-        <div className="Display">
-                {data.imgs.map(item =>(
-                 <img className="Images"></img>
-                 ))}
-        </div>
         </div>
         </div>
     );
