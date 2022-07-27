2e189e86f25698f32a41f3ca0e0e8872d35fef48
kovacsfrank98 <51347588+kovacsfrank98@users.noreply.github.com>
2022-07-27 10:24:40 -0400

    U8: New

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Giphy from './components/Giphy.js';
 function App() {
   return (
     <div className="App"> 
-      <div className="Api">
-      </div>
+        <Giphy>
+        </Giphy>
     </div>
   );
 }
